cd2c005814ded897cf4a39e8be8a6bf79c8b2a61
Nadiia <emmideagorn@gmail.com>
2024-02-16 02:58:30 +0200

    reload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Profile from "./Profile/Profile"
+import Statistics from "./Statistics/Statistics"
+import FriendList from "./FriendList/FriendList"
+
+
+import userData from "../constants/user.json"
+import dataStatistics from "../constants/data.json"
+import friends from "../constants/friends.json"
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template by Nadiya
+    <div>
+      <Profile username={userData.username} tag={userData.tag} location={userData.location} avatar={userData.avatar} stats={userData.stats} />
+      <Statistics title="Upload stats" stats={dataStatistics} />
+      <FriendList friends={friends}/>
     </div>
   );
 };
